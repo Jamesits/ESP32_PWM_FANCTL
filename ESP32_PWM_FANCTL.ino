1b1e81e5453c0518b44657cedc380b0c866d14f2
James Swineson <github@public.swineson.me>
2020-07-26 15:20:56 +0800

    initial commit

---
+++ ESP32_PWM_FANCTL.ino
@@ -0,0 +1,49 @@
+// Simple 4-pin fan controller
+// Copyright (c) 2020 James Swineson. Licensed under the GLWTS license.
+
+#define GPIO_FAN 16
+#define GPIO_LED 5
+#define GPIO_BTN 27
+
+#define PWM_FREQUENCY 25000
+#define PWM_RESOUTION 8 // bits
+
+#define DEBOUNCE_COOLDOWN_MICROS 10000
+
+volatile int percentage = 0;
+volatile int debounce_status = 0;
+// 0: start
+// 1: triggering
+unsigned long debounce_trigger_time;
+
+void IRAM_ATTR btn_down() {
+  debounce_trigger_time = micros();
+  debounce_status = 1;
+}
+
+void setup()
+{
+  pinMode(GPIO_FAN, OUTPUT);
+  pinMode(GPIO_LED, OUTPUT);
+  ledcSetup(0, PWM_FREQUENCY, PWM_RESOUTION);
+  ledcSetup(1, PWM_FREQUENCY, PWM_RESOUTION);
+  ledcAttachPin(GPIO_FAN, 0);
+  ledcAttachPin(GPIO_LED, 1);
+
+  pinMode(GPIO_BTN, INPUT_PULLUP);
+  attachInterrupt(GPIO_BTN, btn_down, FALLING);
+}
+
+void loop() 
+{
+  if (debounce_status == 1 && micros() - debounce_trigger_time > DEBOUNCE_COOLDOWN_MICROS) {
+    debounce_status = 0;
+    percentage += 10;
+    if (percentage > 100) percentage = 0;
+    int dutyCycle = 255 * percentage / 100;
+    ledcWrite(0, dutyCycle);
+    ledcWrite(1, 255-dutyCycle);
+  }
+  
+  delay(10);
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+ESP32 program for controlling 4-pin PC fan. 
+
+Wiring: 
+* Hook GPIO16 to the blue (RPM input) pin of the fan
+* Connect all the GNDs
+* Wire a push button between GPIO27 and GND
+
+Usage:
+Press the button to choose the fan output level between 0%, 10%, 20%, ..., 100%. 
+
+Caveats:
+* 3.3V GPIO works for the fan we tested; but you might need a level shifter
+* Software debouncing for the button is implemented but hardware debouncing is still favored
